9d9a6f47a35e39921c4e7b27863e4e8472b498a0
ArbIsmajli <ismajliarb@gmail.com>
2025-02-03 19:47:03 +0100

    ekrytem

---
+++ dom.js
@@ -0,0 +1,10 @@
+let paragrafi = document.querySelector("p"); //objekt
+// selekton elementin e pare te tipit p
+let paragrafi1 = document.querySelectorAll("p"); //varg
+// selekton te gjitha elementet e tipit p
+
+let paragrafimeklase = document.querySelector(".p2");
+let paragrafimeklase1 = document.getElementsByClassName("p1");
+
+
+console.log(paragrafi);
